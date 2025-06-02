9f244c69b0758b29f85397b99150293998bf078d
Arce Mendivil Carlos Antonio <71289815+AntonioArce123@users.noreply.github.com>
2025-06-01 21:34:11 -0700

    Comentarios_Innecesarios
    Borrando comentarios no necesarios.

--- 8/08_SVM.py
+++ 8/08_SVM.py
@@ -5,25 +5,19 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 
-# Cargar dataset Iris
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# Dividir en entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Escalar características
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Modelo SVM
 modelo = SVC(kernel='linear')
 modelo.fit(X_train, y_train)
 
-# Predicciones
 y_pred = modelo.predict(X_test)
 
-# Resultados
 print(classification_report(y_test, y_pred))
